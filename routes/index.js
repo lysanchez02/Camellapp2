83efb4f0050c71c9b83836d259f41d1363569bed
Leyder Sanchez <lysanchez02@misena.edu.co>
2022-12-06 20:43:05 -0500

    cambios en index

--- routes/index.js
+++ routes/index.js
@@ -16,20 +16,20 @@ const categoriacontroller = require ('../controllers/categoriacontroller');
 //const autenticacioncontroller = require ('../controllers/authcontroller')
 
 
-/*rutas tabla empleador*/
+/*rutas tabla empleador - lisbeth*/
 router.get('/ListarEmpleador', controladorEmpleador.ListarEmpleador);
 router.get('/empleador/:id', controladorEmpleador.ListarId);
 router.post('/GuardarEmpleador', controladorEmpleador.GuardarEmpleador);
 router.delete('/eliminarempleador/:id', controladorEmpleador.EliminarEmpleador);
 
-/*rutas tabla ofertasempleo*/
+/*rutas tabla ofertasempleo - sebastian*/
 
 router.get('/consultaOfertas', controladorofertaempleo.consultaOfertas);
 router.get('/ofertasdeempleo/:id', controladorofertaempleo.ListarId11);
 router.post('/Guardarofertaempleo', controladorofertaempleo.Guardarofertaempleo);
 router.delete('/eliminarofertaempleo/:id', controladorofertaempleo.Eliminarofertaempleo);
 
-/*rutas tabla postulante*/
+/*rutas tabla postulante - leyder*/
 
 router.get('/ListarPostulante', controladorPostulante.ListarPostulante);
 router.get('/postulante/:id', controladorPostulante.ListarId2);
@@ -37,35 +37,35 @@ router.post('/GuardarPostulante', controladorPostulante.GuardarPostulante);
 router.delete('/eliminarpostulante/:id', controladorPostulante.EliminarPostulante);
 
 
-/*rutas tabla multimedia*/
+/*rutas tabla multimedia - jhon*/
 
 router.get('/Listarmultimedia', controladormultimedia.Listarmultimedia);
 router.get('/multimedia/:id', controladormultimedia.ListarId5);
 router.post('/Guardarmultimedia', controladormultimedia.Guardarmultimedia);
 router.delete('/Eliminarmultimedia/:id', controladormultimedia.Eliminarmultimedia);
 
-/*rutas tabla postulaciones*/
+/*rutas tabla postulaciones - eduardo*/
 
 router.get('/Listarpostulaciones', controladorPostulaciones.Listarpostulaciones);
 router.get('/postulaciones/:id', controladorPostulaciones.ListarId3);
 router.post('/Guardarpostulaciones', controladorPostulaciones.Guardarpostulaciones);
 router.delete('/eliminarpostulaciones/:id', controladorPostulaciones.Eliminarpostulacion);
 
-/*rutas tabla registro catalogo*/
+/*rutas tabla registro catalogo maria */
 
 router.get('/Listarregistrocatalogo', controladorregistrocatalogo.registrocatalogo);
 router.get('/registrocatalogo/:id', controladorregistrocatalogo.ListarId4);
 router.post('/Guardarregistrocatalogo', controladorregistrocatalogo.Guardarregistrocatalogo);
 router.delete('/eliminarregistrocatalogo/:id', controladorregistrocatalogo.Eliminarregistrocatalogo);
 
-/*rutas tabla usuario*/
+/*rutas tabla usuario - javier*/
 
 router.get('/Listarusuario', controladorusuario.Listarusuario);
 router.get('/usuario/:id', controladorusuario.ListarId6);
 router.post('/Guardarusuario', controladorusuario.Guardarusuario);
 router.delete('/Eliminarusuario/:id', controladorusuario.Eliminarusuario);
 
-/*rutas tabla categoria*/
+/*rutas tabla categoria leyder*/
 router.get('/Listarcategorianew',categoriacontroller.Listarcategorianew);
 router.post('/guardarcategoria',categoriacontroller.guardarcategoria);
 router.get('/categoria/:id', categoriacontroller.ListarIdcategoria);
